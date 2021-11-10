ccd131cc741cd0ef7ca2a0bf05a70dd57a07d5b6
SlothPete7773 <Veerakit413@gmail.com>
2021-11-10 13:19:40 +0700

    Change heroku configuration

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from logging import debug
-import numpy as np
 from flask import Flask, render_template, request
-import pickle
 from joblib import load
 
 # Initialize flask app
--- logistic-model.py
+++ logistic-model.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from joblib import load, dump
+from joblib import dump
 
 
 # Import dataset
